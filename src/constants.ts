8c08f45825745cf8bad68cd970a689ad7399a0c6
Jay Alfaras <alfarasjb@gmail.com>
2024-06-29 10:55:48 +0800

    organized some stuff

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export enum ApiEndpoints {
+    DB_STORE = '/api/db/store',
+    DB_MONTHLY_DATA = '/api/db/monthly-data',
+    DB_HISTORY = '/api/db/history'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import express, { Request, Response } from "express"; 
+import express, { Request, Response } from "express";  
 import dotenv from "dotenv"; 
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'; 
+import { ApiEndpoints } from './constants';
 
 dotenv.config() 
 
 const app = express() 
 const port = process.env.PORT 
+
 app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => { 
@@ -18,7 +20,7 @@ app.post('/test', (req: Request, res: Response) => {
     res.status(200).json({message: "This is a test endpoint"})
 }) 
 
-app.post('/api/db/store', (req: Request, res: Response) => {
+app.post(ApiEndpoints.DB_STORE, (req: Request, res: Response) => {
     // Call this when storing to database
     console.log("Storing to database.") 
     console.log("Payload: ", req.body) 
@@ -26,11 +28,22 @@ app.post('/api/db/store', (req: Request, res: Response) => {
     res.status(200).json({message: "Stored to database: " + req.body})
  })
 
-app.get('/api/db/monthly-data', (req: Request, res: Response) => {
+app.get(ApiEndpoints.DB_MONTHLY_DATA, (req: Request, res: Response) => {
     // Call this when generating a monthly report
     console.log("Getting data") 
+    // Payload 
+    // Get start and end date 
+    const { start_date, end_date } = req.body   
+    const message = `Getting monthly data from ${start_date} to ${end_date}`
+    console.log(message)
     // Get data from db here 
     res.status(200).json({message: "Getting data from database"})
+})
+
+app.get(ApiEndpoints.DB_HISTORY, (req: Request, res: Response) => {
+    // Call this when getting history data 
+    console.log("Getting history data") 
+    res.status(200).json({message: "Getting history data"})
 })
 
 app.listen(port, () => {
