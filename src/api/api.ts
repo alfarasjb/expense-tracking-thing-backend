b822b3030d10a2d6d2531fcb3d1578afa608c108
Jay Alfaras <alfarasjb@gmail.com>
2024-07-03 12:48:49 +0800

    added winston logging

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,9 +2,21 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv"; 
 import bodyParser from 'body-parser'; 
 import { ApiEndpoints } from '../definitions/constants'; 
-import DatabaseManager from '../database/database';
+import DatabaseManager from '../database/database'; 
+import winston, { log } from 'winston';
 
 dotenv.config() 
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(), 
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console(),
+    ],
+})
 
 class Server {
     private app: express.Application; 
@@ -23,27 +35,30 @@ class Server {
 
 
     private setupRoutes() {
-        this.app.get('/', (req: Request, res: Response) => {
-            res.status(200).json({message: "Welcome"}) 
+        this.app.get('/', (req: Request, res: Response) => { 
+            const message = "Welcome"
+            logger.info(message)
+            res.status(200).json({message: message}) 
         }) 
 
         this.app.post(ApiEndpoints.DB_STORE, (req: Request, res: Response) => {
-            // Call this when storing to database 
-            console.log("Storing to database.") 
-            console.log("Payload: ", req.body) 
+            // Call this when storing to database   
+            const message = `Storing to database. Payload: ${req.body}` 
+            logger.info(message)
             // Store to db here 
             this.databaseManager.storeExpenseData(req.body) 
             res.status(200).json({message: "Stored to database: " + req.body})
         }) 
         
         this.app.get(ApiEndpoints.DB_MONTHLY_DATA, (req: Request, res: Response) => {
-            // Call this when generating a monthly report 
-            console.log("Getting data")  
+            // Call this when generating a monthly report  
+            const logger_message = "Getting data"
+            logger.info(logger_message)  
             // Payload 
             // Get start and end date
             const { startDate, endDate } = req.body 
             const message = `Getting monthly data from ${startDate} to ${endDate}` 
-            console.log(message)
+            logger.info(message)
             // Get data from db here 
             this.databaseManager.getExpenseDataFromDates(startDate, endDate)
             res.status(200).json({message: "Getting data from database"})
@@ -51,8 +66,9 @@ class Server {
 
         this.app.get(ApiEndpoints.DB_HISTORY, (req: Request, res: Response) => {
             // Call this when getting history data 
-            const { username } = req.body
-            console.log("Getting history data") 
+            const { username } = req.body 
+            const logger_message = "Getting history data"
+            logger.info(logger_message)  
             this.databaseManager.getAllExpenseData(username).then((expenseData) => {
                 if (expenseData.length > 0) {
                     res.status(200).json({message: "Fetching history data.", data: expenseData}) 
@@ -64,14 +80,16 @@ class Server {
 
         this.app.post(ApiEndpoints.DB_CLEAR, (req: Request, res: Response) => {
             // Call this when clearing databse 
-            console.log("Clearing database") 
+            const logger_message = "Clearing database"
+            logger.info(logger_message)
             this.databaseManager.clearDatabase()
             res.status(200).json({message: "Database cleared"})
         }) 
         
         this.app.post(ApiEndpoints.DB_AUTH_REGISTER, async (req: Request, res: Response) => {
             // Call this when registering a user 
-            console.log("Registering user")   
+            const logger_message = "Registering user" 
+            logger.info(logger_message)   
             this.databaseManager.registerUser(req.body).then((registered) => {
                 if (registered) {
                     res.status(200).json({message: `User ${req.body.username} registered succesfully.`})
@@ -82,8 +100,9 @@ class Server {
         }) 
 
         this.app.post(ApiEndpoints.DB_AUTH_LOGIN, async (req: Request, res: Response) => {
-            // Call this when logging in a user 
-            console.log("Logging in") 
+            // Call this when logging in a user  
+            const logger_message = "Logging in" 
+            logger.info(logger_message)
             this.databaseManager.loginUser(req.body).then((success) => {
                 if (success) {
                     res.status(200).json({message: "Logged in successfully."}) 
