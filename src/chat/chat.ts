a44c5abda4fce2625de66434e8b9096e557a37ab
Jay Alfaras <alfarasjb@gmail.com>
2024-07-06 14:06:21 +0800

    added historical context to chatbot feature

--- src/chat/chat.ts
+++ src/chat/chat.ts
@@ -9,12 +9,13 @@ import { ExpenseJson } from "../database/templates";
 dotenv.config()
 
 // TODO: Improve prompts 
-// TODO: Add history 
 class ChatBot {
-    private openai: OpenAI  
+    private openai: OpenAI 
+    private chatHistory: OpenAI.Chat.ChatCompletionMessageParam[]
 
-    constructor() {
-        this.openai = new OpenAI() 
+    constructor() { 
+        this.openai = new OpenAI()  
+        this.chatHistory = []
     } 
     private convertExpenseJsonToString(expenseData: ExpenseJson[]): string {
         return JSON.stringify(expenseData, null, 2);
@@ -30,12 +31,24 @@ class ChatBot {
         return response
     } 
 
-    async sendMessageToChatBot(userMessage: string, data: ExpenseJson[]): Promise<string | null> {
-        const messages: OpenAI.Chat.ChatCompletionMessageParam[] = this.createMessage(
-            USER_CHAT_SYSTEM_PROMPT_TEMPLATE, 
-            genericUserChatPromptTemplate(this.convertExpenseJsonToString(data), userMessage)
-        )
-        return this.chat(messages)
+    async sendMessageToChatBot(userMessage: string, data: ExpenseJson[]): Promise<string | null> {  
+        /*
+        Check if there is history 
+        If there is history, get last 6, and add the user message (exclude the system prompt) 
+        
+        If there is no history, create new messages 
+        */ 
+       let messages = []
+       if (this.chatHistory.length > 0) {
+            // history has contents 
+            const chatHist = this.chatHistory.slice(-6)  // Get last 6 messages 
+            const histAndMessage: OpenAI.Chat.ChatCompletionMessageParam[] = chatHist.concat([{ role: "user", content: userMessage }])   
+            messages = histAndMessage
+       } else {
+            const messageToChatBot = genericUserChatPromptTemplate(this.convertExpenseJsonToString(data), userMessage) 
+            messages = this.createMessage(USER_CHAT_SYSTEM_PROMPT_TEMPLATE, messageToChatBot) 
+       } 
+       return this.chat(messages) 
     }
 
     private createMessage(systemPrompt: string, userPrompt: string): OpenAI.Chat.ChatCompletionMessageParam[] {
@@ -57,7 +70,9 @@ class ChatBot {
             messages: messages,
             model: ChatModels.GPT_3_5_TURBO
         })
-        return completion.choices[0].message.content;
+        const chatbotResponse = completion.choices[0].message.content 
+        this.chatHistory = messages.concat([{ role: "assistant", content: chatbotResponse }]) 
+        return chatbotResponse
     }
 }
 
